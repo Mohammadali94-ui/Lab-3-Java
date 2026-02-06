9ec5730aaa6baeb5cf6aebde84e347eb2756654d
Mohammad Ali Bokhari <mabokhari14@gmail.com>
2026-02-06 09:22:26 -0500

    Completed PayrollTest driver and verified output

--- PayrollTest.java
+++ PayrollTest.java
@@ -5,5 +5,11 @@ public class PayrollTest {
 
     public static void main(String[] args) {
 
+        CommissionEmployee employee =
+                new CommissionEmployee("Mohammad", "Bokhari",
+                        "123-45-6789", 5000.00, 0.10);
+
+        System.out.println(employee);
+        System.out.println("Earnings: $" + employee.earnings());
     }
 }
