6b739261e9958435d12b7328e4c0cc2a97bdcae6
Mohammad Ali Bokhari <mabokhari14@gmail.com>
2026-02-06 08:53:32 -0500

    Completed Employee constructor, getters, abstract earnings, and toString

--- Employee.java
+++ Employee.java
@@ -1,3 +1,36 @@
-public class Employee {
-    
+public abstract class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    // Constructor
+    public Employee(String firstName, String lastName, String socialSecurityNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    // Abstract method
+    public abstract double earnings();
+
+    // toString override
+    @Override
+    public String toString() {
+        return firstName + " " + lastName +
+                "\nSSN: " + socialSecurityNumber;
+    }
 }
